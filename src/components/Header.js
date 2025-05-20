a5109501605419b93fa1a204a52c6472d16e386b
meaza-ketaw <102231418+meaza-ketaw@users.noreply.github.com>
2025-05-20 09:24:48 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      {/* <h1>Netflix</h1>
-      <nav>
-        <Link to="/"></Link>
-      </nav> */}
+      
     </header>
   );
 };
